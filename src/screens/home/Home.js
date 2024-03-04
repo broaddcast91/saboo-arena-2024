0c9b03cfd04081e366c0caa5206f2719100ed6a4
iamharnad <haranathpriyan@gmail.com>
2024-03-04 12:38:36 +0530

    added alignment, styling and colours in home page and changed form layout with colours in just alto 800 page

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -5,9 +5,9 @@ import MobileSlider from '../../components/home/MobileSlider';
 import Cars from '../../components/home/Cars';
 // import About from '../../components/home/About';
 // import Accomplishments from '../../components/home/Accomplishments';
-// import { FaStar } from 'react-icons/fa';
+import { FaStar } from 'react-icons/fa';
 // Import Swiper React components
-// import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from 'swiper/react';
 //confetti
 // import Confetti from 'react-confetti';
 
@@ -15,8 +15,8 @@ import Cars from '../../components/home/Cars';
 import 'swiper/css';
 import 'swiper/css/navigation';
 // import required modules
-// import { Autoplay } from 'swiper';
-// import { reviews } from '../../constants/carservice';
+import { Autoplay } from 'swiper';
+import { reviews } from '../../constants/carservice';
 import Header from '../../components/header/Header';
 
 import { Helmet } from 'react-helmet';
@@ -324,158 +324,158 @@ const Blog = () => {
 
 const Testimonials = () => {
   return (
-    <section
-      class='flex items-center justify-center py-20 bg-white min-w-screen tails-selected-element'
-      contenteditable='true'
-    >
-      <div class='px-16 bg-white'>
-        <div class='container flex flex-col items-start mx-auto lg:items-center'>
-          <p
-            class='relative flex items-start justify-start w-full text-lg font-bold tracking-wider text-gray-500 uppercase lg:justify-center lg:items-center'
-            data-primary='purple-500'
-          >
-            Don't just take our word for it
-          </p>
+    // <section
+    //   class='flex items-center justify-center py-20 bg-white min-w-screen tails-selected-element'
+    //   contenteditable='true'
+    // >
+    //   <div class='px-16 bg-white'>
+    //     <div class='container flex flex-col items-start mx-auto lg:items-center'>
+    //       <p
+    //         class='relative flex items-start justify-start w-full text-lg font-bold tracking-wider text-gray-500 uppercase lg:justify-center lg:items-center'
+    //         data-primary='purple-500'
+    //       >
+    //         Don't just take our word for it
+    //       </p>
 
-          <h2 class='relative flex items-start justify-start w-full max-w-3xl text-5xl font-bold lg:justify-center text-[#293e91]'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              fill='currentColor'
-              class='absolute right-0 hidden w-12 h-12 -mt-2 -mr-16 text-gray-200 lg:inline-block'
-              viewBox='0 0 975.036 975.036'
-            >
-              <path
-                d='M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z'
-                class=''
-              ></path>
-            </svg>
-            See what others are saying
-          </h2>
-          <div
-            class='block w-full h-0.5 max-w-lg mt-6 bg-blue-100 rounded-full'
-            data-primary='purple-600'
-          ></div>
+    //       <h2 class='relative flex items-start justify-start w-full max-w-3xl text-5xl font-bold lg:justify-center text-[#293e91]'>
+    //         <svg
+    //           xmlns='http://www.w3.org/2000/svg'
+    //           fill='currentColor'
+    //           class='absolute right-0 hidden w-12 h-12 -mt-2 -mr-16 text-gray-200 lg:inline-block'
+    //           viewBox='0 0 975.036 975.036'
+    //         >
+    //           <path
+    //             d='M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z'
+    //             class=''
+    //           ></path>
+    //         </svg>
+    //         See what others are saying
+    //       </h2>
+    //       <div
+    //         class='block w-full h-0.5 max-w-lg mt-6 bg-blue-100 rounded-full'
+    //         data-primary='purple-600'
+    //       ></div>
 
-          <div class='items-center justify-center w-full mt-12 mb-4 lg:flex'>
-            <div class='flex flex-col items-start justify-start w-full h-auto mb-12 lg:w-1/3 lg:mb-0'>
-              <div class='flex items-center justify-center'>
-                <div class='w-16 h-16 mr-4 overflow-hidden bg-gray-200 rounded-full'>
-                  <img
-                    src='https://img.freepik.com/free-photo/worldface-pakistani-guy-white-background_53876-146312.jpg?w=1800&t=st=1709009205~exp=1709009805~hmac=e33e8852ba20c7cc42a18e39554d4fe73af8435dd124bcd383e526e67856617c'
-                    class='object-cover w-full h-full'
-                    alt='review-1'
-                  />
-                </div>
-                <div class='flex flex-col items-start justify-center'>
-                  <h4 class='font-bold text-[#293e91] text-xl'>Vijay</h4>
-                  <p class='text-gray-500 text-sm'>Engineer</p>
-                </div>
-              </div>
-              <blockquote class='mt-8 text-md italic text-gray-500'>
-                "Wonderfull experinece with staff and Mr Prasad Induri has been
-                very good in througout the process. Delivered as promised. Thank
-                you RKS"
-              </blockquote>
-            </div>
-            <div class='flex flex-col items-start justify-start w-full h-auto px-0 mx-0 mb-12 border-l border-r border-transparent lg:w-1/3 lg:mb-0 lg:px-8 lg:mx-8 lg:border-gray-200'>
-              <div class='flex items-center justify-center'>
-                <div class='w-16 h-16 mr-4 overflow-hidden bg-gray-200 rounded-full'>
-                  <img
-                    src='https://img.freepik.com/free-photo/arab-man-with-beard-standing-bricks-wall-background-with-happy-cool-smile-face-lucky-person_839833-31933.jpg?w=1800&t=st=1709009424~exp=1709010024~hmac=7855a6e03e1e53e9542bb5f9d739d731a956a4207c2ba177289337d0e149178e'
-                    class='object-cover w-full h-full'
-                    alt='testimonial-profile-person-2'
-                  />
-                </div>
-                <div class='flex flex-col items-start justify-center'>
-                  <h4 class='font-bold text-[#293e91] text-xl'>Mohan Reddy</h4>
-                  <p class='text-gray-500 text-sm'>Doctor</p>
-                </div>
-              </div>
-              <blockquote class='mt-8 text-md italic text-gray-500'>
-                "Mr. Venkat Reddy is good guy and very co ordinated and given
-                all his best and excellent service to me I will refer my friends
-                and family to rks motors tardbund."
-              </blockquote>
-            </div>
-            <div class='flex flex-col items-start justify-start w-full h-auto lg:w-1/3'>
-              <div class='flex items-center justify-center'>
-                <div class='w-16 h-16 mr-4 overflow-hidden bg-gray-200 rounded-full'>
-                  <img
-                    src='https://img.freepik.com/free-photo/smiling-businessman-face-portrait-wearing-suit_53876-148138.jpg?w=1800&t=st=1709009087~exp=1709009687~hmac=be37fd25992f7bef82371504fbb517783f7f25fa98e40be1d8baf29d3f18c50d'
-                    class='object-cover w-full h-full'
-                    alt='review-3'
-                  />
-                </div>
-                <div class='flex flex-col items-start justify-center'>
-                  <h4 class='font-bold text-xl text-[#293e91]'>
-                    Jagadish Kumar
-                  </h4>
-                  <p class='text-gray-500 text-sm'>Business Man</p>
-                </div>
-              </div>
-              <blockquote class='mt-8 text-md italic text-gray-500'>
-                "Excellent Work from Sales Person Mr. Tirupati happy with rks
-                motors tadbund showroom Receiving was excellent 👍 all covid
-                saftey taken at showroom i really awsome all staff 😍👌💯 i will
-                prefer to rks motors tadbund thank you 🙏"
-              </blockquote>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-    // <div className='container mx-auto my-5'>
-    //   <p className='text-center mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-[#293e91]'>
-    //     Testimonials
-    //   </p>
-    //   <p className='mb-8 text-center font-extralight'>
-    //     Discover what others have to say
-    //   </p>
-    //   <Swiper
-    //     slidesPerView={1}
-    //     loop={true}
-    //     autoplay={{ delay: 5000, disableOnInteraction: false }}
-    //     modules={[Autoplay]}
-    //     breakpoints={{
-    //       640: {
-    //         slidesPerView: 2,
-    //         spaceBetween: 20,
-    //       },
-    //       768: {
-    //         slidesPerView: 2,
-    //         spaceBetween: 40,
-    //       },
-    //       1024: {
-    //         slidesPerView: 3,
-    //         spaceBetween: 50,
-    //       },
-    //     }}
-    //     className='mySwiper'
-    //   >
-    //     {reviews.map((item, index) => (
-    //       <SwiperSlide key={index}>
-    //         <div className='px-8'>
-    //           <img
-    //             src='https://images-saboomaruti-in.s3.ap-south-1.amazonaws.com/saboonexa/icons/Saboo-Nexa-Profile-Icon.webp'
-    //             className='mx-auto'
-    //             alt='offers'
-    //           />
-    //           <p className='text-center text-base text-blue-800 font-semibold py-2'>
-    //             {item.author}
-    //           </p>
-    //           <div className='flex items-center justify-center space-x-1'>
-    //             <FaStar className='text-orange-400' />
-    //             <FaStar className='text-orange-400' />
-    //             <FaStar className='text-orange-400' />
-    //             <FaStar className='text-orange-400' />
-    //             <FaStar className='text-orange-400' />
+    //       <div class='items-center justify-center w-full mt-12 mb-4 lg:flex'>
+    //         <div class='flex flex-col items-start justify-start w-full h-auto mb-12 lg:w-1/3 lg:mb-0'>
+    //           <div class='flex items-center justify-center'>
+    //             <div class='w-16 h-16 mr-4 overflow-hidden bg-gray-200 rounded-full'>
+    //               <img
+    //                 src='https://img.freepik.com/free-photo/worldface-pakistani-guy-white-background_53876-146312.jpg?w=1800&t=st=1709009205~exp=1709009805~hmac=e33e8852ba20c7cc42a18e39554d4fe73af8435dd124bcd383e526e67856617c'
+    //                 class='object-cover w-full h-full'
+    //                 alt='review-1'
+    //               />
+    //             </div>
+    //             <div class='flex flex-col items-start justify-center'>
+    //               <h4 class='font-bold text-[#293e91] text-xl'>Vijay</h4>
+    //               <p class='text-gray-500 text-sm'>Engineer</p>
+    //             </div>
     //           </div>
-    //           <p className='text-sm text-center'>{item.text}</p>
+    //           <blockquote class='mt-8 text-md italic text-gray-500'>
+    //             "Wonderfull experinece with staff and Mr Prasad Induri has been
+    //             very good in througout the process. Delivered as promised. Thank
+    //             you RKS"
+    //           </blockquote>
     //         </div>
-    //       </SwiperSlide>
-    //     ))}
-    //   </Swiper>
-    // </div>
+    //         <div class='flex flex-col items-start justify-start w-full h-auto px-0 mx-0 mb-12 border-l border-r border-transparent lg:w-1/3 lg:mb-0 lg:px-8 lg:mx-8 lg:border-gray-200'>
+    //           <div class='flex items-center justify-center'>
+    //             <div class='w-16 h-16 mr-4 overflow-hidden bg-gray-200 rounded-full'>
+    //               <img
+    //                 src='https://img.freepik.com/free-photo/arab-man-with-beard-standing-bricks-wall-background-with-happy-cool-smile-face-lucky-person_839833-31933.jpg?w=1800&t=st=1709009424~exp=1709010024~hmac=7855a6e03e1e53e9542bb5f9d739d731a956a4207c2ba177289337d0e149178e'
+    //                 class='object-cover w-full h-full'
+    //                 alt='testimonial-profile-person-2'
+    //               />
+    //             </div>
+    //             <div class='flex flex-col items-start justify-center'>
+    //               <h4 class='font-bold text-[#293e91] text-xl'>Mohan Reddy</h4>
+    //               <p class='text-gray-500 text-sm'>Doctor</p>
+    //             </div>
+    //           </div>
+    //           <blockquote class='mt-8 text-md italic text-gray-500'>
+    //             "Mr. Venkat Reddy is good guy and very co ordinated and given
+    //             all his best and excellent service to me I will refer my friends
+    //             and family to rks motors tardbund."
+    //           </blockquote>
+    //         </div>
+    //         <div class='flex flex-col items-start justify-start w-full h-auto lg:w-1/3'>
+    //           <div class='flex items-center justify-center'>
+    //             <div class='w-16 h-16 mr-4 overflow-hidden bg-gray-200 rounded-full'>
+    //               <img
+    //                 src='https://img.freepik.com/free-photo/smiling-businessman-face-portrait-wearing-suit_53876-148138.jpg?w=1800&t=st=1709009087~exp=1709009687~hmac=be37fd25992f7bef82371504fbb517783f7f25fa98e40be1d8baf29d3f18c50d'
+    //                 class='object-cover w-full h-full'
+    //                 alt='review-3'
+    //               />
+    //             </div>
+    //             <div class='flex flex-col items-start justify-center'>
+    //               <h4 class='font-bold text-xl text-[#293e91]'>
+    //                 Jagadish Kumar
+    //               </h4>
+    //               <p class='text-gray-500 text-sm'>Business Man</p>
+    //             </div>
+    //           </div>
+    //           <blockquote class='mt-8 text-md italic text-gray-500'>
+    //             "Excellent Work from Sales Person Mr. Tirupati happy with rks
+    //             motors tadbund showroom Receiving was excellent 👍 all covid
+    //             saftey taken at showroom i really awsome all staff 😍👌💯 i will
+    //             prefer to rks motors tadbund thank you 🙏"
+    //           </blockquote>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </section>
+    <div className='container mx-auto my-5'>
+      <p className='text-center mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-[#293e91]'>
+        Testimonials
+      </p>
+      <p className='mb-8 text-center font-extralight'>
+        Discover what others have to say
+      </p>
+      <Swiper
+        slidesPerView={1}
+        loop={true}
+        autoplay={{ delay: 5000, disableOnInteraction: false }}
+        modules={[Autoplay]}
+        breakpoints={{
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+          },
+        }}
+        className='mySwiper'
+      >
+        {reviews.map((item, index) => (
+          <SwiperSlide key={index}>
+            <div className='px-8'>
+              <img
+                src='https://images-saboomaruti-in.s3.ap-south-1.amazonaws.com/saboonexa/icons/Saboo-Nexa-Profile-Icon.webp'
+                className='mx-auto'
+                alt='offers'
+              />
+              <p className='text-center text-base text-blue-800 font-semibold py-2'>
+                {item.author}
+              </p>
+              <div className='flex items-center justify-center space-x-1'>
+                <FaStar className='text-orange-400' />
+                <FaStar className='text-orange-400' />
+                <FaStar className='text-orange-400' />
+                <FaStar className='text-orange-400' />
+                <FaStar className='text-orange-400' />
+              </div>
+              <p className='text-sm text-center'>{item.text}</p>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
   );
 };
 
--- src/screens/home/HomePageTab.js
+++ src/screens/home/HomePageTab.js
@@ -20,7 +20,7 @@ const HomePageTab = () => {
     <>
       <div className='container mx-auto mt-8'>
         <Tab.Group>
-          <Tab.List className='flex p-1 space-x-1 bg-gray-100 rounded-full'>
+          <Tab.List className='flex p-1 space-x-1 rounded-full'>
             <Tab
               className={({ selected }) =>
                 `${
@@ -112,7 +112,7 @@ const ArenaCars = () => {
       <span className='font-semibold'>22.05 kmpl*</span>
     </p>
   </div> */}
-                  <p className='absolute bottom-3 left-3 text-blue-900 font-normal'>
+                  <p className='absolute bottom-3 right-3 text-blue-900 font-normal text-sm'>
                     <svg
                       xmlns='http://www.w3.org/2000/svg'
                       className='h-4 w-4 inline-block mr-1'
@@ -178,26 +178,13 @@ const ArenaCars = () => {
                     VIEW OFFER
                   </Link>
 
-                  <a
-                    href={item.brouchure}
-                    target='_blank'
+                  <Link
+                    to={item.explore}
                     rel='noreferrer'
                     className='border border-blue-900 text-blue-900 hover:border-red-700 hover:text-white hover:bg-red-700 p-2 max-w-[44%] w-full flex justify-center items-center text-sm uppercase rounded-full'
                   >
-                    <svg
-                      xmlns='http://www.w3.org/2000/svg'
-                      className='h-4 w-4 mr-1'
-                      viewBox='0 0 20 20'
-                      fill='currentColor'
-                    >
-                      <path
-                        fillRule='evenodd'
-                        d='M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V7l-4-4H4zm2 0v10h12V5l-4-4H6zm6 4.5a.5.5 0 00-.5.5v4a.5.5 0 001 0v-4a.5.5 0 00-.5-.5zM8 9a.5.5 0 00-.5.5v3a.5.5 0 001 0v-3A.5.5 0 008 9zm4 0a.5.5 0 00-.5.5v3a.5.5 0 001 0v-3a.5.5 0 00-.5-.5z'
-                        clipRule='evenodd'
-                      />
-                    </svg>
-                    Brochure
-                  </a>
+                    explore
+                  </Link>
                 </div>
               </div>
             </div>
@@ -220,38 +207,85 @@ const ArenaCars = () => {
 const CNGCars2 = () => {
   return (
     <>
-      <div className='grid sm:grid-cols-2 lg:grid-cols-3 gap-4 container mx-auto px-5 pb-5'>
+      <div className='grid sm:grid-1 lg:grid-cols-3 gap-2 container mx-auto pb-5'>
         {cngProducts.map((item, index) => (
           <div
-            className='flex flex-col bg-[#eee] rounded shadow-lg border relative overflow-hidden'
+            className='flex flex-col bg-white rounded-md shadow-md border border-gray-300 hover:shadow-lg  transform transition duration-500 hover:scale-106'
             key={index}
           >
             <div className='flex-auto overflow-hidden'>
               <Link to={item.explore}>
-                <img
-                  src={item.img}
-                  alt={item.name}
-                  className='mx-auto product_image h-[250px] lg:h-[310px] w-full object-cover'
-                />
+                <div className='relative'>
+                  {' '}
+                  <img
+                    src={item.img}
+                    alt={item.name}
+                    className='mx-auto product_image lg:h-[250px] h-[250px] w-full object-cover object-center rounded-t-md'
+                  />
+                  <p className='absolute shadow-xl top-3 -right-7 uppercase text-sm text-center text-white rotate-45 bg-green-600 py-1 w-28'>
+                    CNG
+                  </p>
+                  <p className='absolute bottom-3 right-3 text-blue-900 font-normal text-sm'>
+                    <svg
+                      xmlns='http://www.w3.org/2000/svg'
+                      className='h-4 w-4 inline-block mr-1'
+                      viewBox='0 0 20 20'
+                      fill='currentColor'
+                    >
+                      <path
+                        fillRule='evenodd'
+                        d='M14.452 7.34a1 1 0 01-.636-.636l-4-7a1 1 0 011.732-1l4 7a1 1 0 01-.636 1.636z'
+                        clipRule='evenodd'
+                      />
+                      <path
+                        fillRule='evenodd'
+                        d='M9 8a1 1 0 011-1h1a1 1 0 010 2H10a1 1 0 01-1-1z'
+                        clipRule='evenodd'
+                      />
+                      <path
+                        fillRule='evenodd'
+                        d='M14.5 10a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zm-3.946-1.895a1 1 0 01-.604-.303l-3-3a1 1 0 011.414-1.414l3 3a1 1 0 01-.81 1.717z'
+                        clipRule='evenodd'
+                      />
+                      <path
+                        fillRule='evenodd'
+                        d='M15 15a1 1 0 01-1 1h-1a1 1 0 110-2h1a1 1 0 011 1z'
+                        clipRule='evenodd'
+                      />
+                      <path
+                        fillRule='evenodd'
+                        d='M10 18a8 8 0 100-16 8 8 0 000 16zm0-2a6 6 0 100-12 6 6 0 000 12zm0-2a4 4 0 100-8 4 4 0 000 8zm0-2a2 2 0 100-4 2 2 0 000 4z'
+                        clipRule='evenodd'
+                      />
+                    </svg>
+                    {item.mileage} km/L
+                  </p>
+                  <div className='absolute inset-0 opacity-10 rounded-t-md'></div>
+                </div>
               </Link>
             </div>
-            <div className='flex-auto bg-white'>
+            <div className='flex-auto bg-white rounded-b-md'>
               <div>
                 <div className='flex items-center justify-between p-5 '>
-                  <img src={item.logo} alt={item.name} className='h-5' />
+                  <img src={item.logo} alt={item.name} className='h-5 w-auto' />
                   <div>
-                    <p className='flex items-center font-bold'>
-                      <BiRupee />
+                    <p className='flex items-center font-bold text-xl'>
+                      <span className='text-xs'>
+                        {' '}
+                        <BiRupee />
+                      </span>
                       &nbsp;{item.price}
-                      <sup>*</sup>
+                      <span className='font-extralight'>
+                        <sup>*</sup>
+                      </span>
                     </p>{' '}
-                    <p className='font-light'>ex-showroom</p>
+                    <p className='font-extralight text-xs mx-1'>ex-showroom</p>
                   </div>
                 </div>
-                <div className='flex items-center justify-center space-x-3 pb-5'>
+                <div className='flex items-center justify-center space-x-2 p-2.5 mb-2'>
                   <Link
                     to={item.explore}
-                    className='bg-green-700 hover:bg-red-700 hover:border hover:text-red duration-300 ease-in text-white p-2 text-sm uppercase w-full max-w-[44%] text-center rounded-full'
+                    className='hover:bg-green-700 bg-blue-800 hover:border hover:text-red duration-300 ease-in text-white p-2 text-sm uppercase w-full max-w-[44%] text-center rounded-full'
                   >
                     Explore
                   </Link>
